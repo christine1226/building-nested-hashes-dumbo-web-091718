b25146df49753b6284ffb4b9be245cb602abeaa2
christine gomez <christine.gomez1226@gmail.com>
2018-09-07 01:45:46 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet"
+        :name => "Lord Capulet",
         :age => "50"
         },
       :matriarch => {
@@ -26,8 +26,8 @@ def third_challenge
         :age => "51"
         },
       :heroine => {
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
